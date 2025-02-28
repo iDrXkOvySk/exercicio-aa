63f95600339936f627b0c8da47f78998b28f13f6
alane@Adrian <adrian.caldeira@aluno.imepac.edu.br>
2025-02-27 21:02:06 -0300

    Comitando método de soma

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public class Main {
 
+    public static int somaDoisNumeros(int n1, int n2){
+      return n1 + n2;
+
+    }
+    public static void main(String[] args) {
+        int resultado = somaDoisNumeros(3, 4);
+        System.out.println("A soma é : " + resultado);
+    }
 }
