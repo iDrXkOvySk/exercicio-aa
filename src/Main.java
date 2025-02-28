7b56593593ea5dc1dafd32f6800a81fefba5f58c
alane@Adrian <adrian.caldeira@aluno.imepac.edu.br>
2025-02-27 21:18:56 -0300

    Comitando número inteiro

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-
+    public static boolean isPar(int numero){
+        return numero % 2 == 0;
+    }
+    public static void main(String[] args) {
+        int numero = 6;
+        boolean resultado = isPar(numero);
+        System.out.println("o número " + numero + " é par : " + resultado);
+    }
 }
