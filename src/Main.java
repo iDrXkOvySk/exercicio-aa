0e607466694bdcd8c4019c96df24fc4d67f8d921
alane@Adrian <adrian.caldeira@aluno.imepac.edu.br>
2025-02-28 18:32:14 -0300

    Comitando Contador de Vogais

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,24 @@
 public class Main {
 
+    public static int contarVogais(String str){
+        int contador = 0;
+
+        str = str.toLowerCase();
+
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        String texto = "Exemplo de contagem de vogais";
+
+        int quantidadeVogais = contarVogais(texto);
+        System.out.println("Quantidade de vogais : " + quantidadeVogais);
+    }
 }
