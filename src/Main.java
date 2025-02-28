ed5681294bf333727fcda7d7bfa94db084865f2d
alane@Adrian <adrian.caldeira@aluno.imepac.edu.br>
2025-02-28 18:22:30 -0300

    Comitando Cálculo de Média dos números

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
 public class Main {
 
+    public static double calcularMedia(double[] numeros){
+        double soma = 0;
+
+        for (double numero : numeros){
+            soma += numero;
+        }
+        return soma / numeros.length;
+    }
+    public static void main(String[] args) {
+        double[] numeros = {10,20,30,40,50,60,70,80,90,100};
+
+        double media = calcularMedia(numeros);
+        System.out.println("A média dos números é : " + media);
+    }
 }
