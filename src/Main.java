dfd892e542867e818e600567c6c6748066868c70
alane@Adrian <adrian.caldeira@aluno.imepac.edu.br>
2025-02-28 18:01:56 -0300

    Comitando inversão de String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
 
+    public static String inverterString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        String texto = "Exemplo de String";
+        String textoInvertido = inverterString(texto);
+
+        System.out.println("Texto original: " + texto);
+        System.out.println("Texto invertido: " + textoInvertido);
+    }
 }
